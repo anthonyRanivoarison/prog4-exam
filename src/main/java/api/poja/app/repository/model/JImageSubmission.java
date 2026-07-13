df7356f9c1f5d691501e4035188f2d530baac555
anthonyRanivoarison <antonitsiory@gmail.com>
2026-07-13 08:21:29 +0300

    feat: create model ImageSubmission and JPA entity

---
+++ src/main/java/api/poja/app/repository/model/JImageSubmission.java
@@ -0,0 +1,25 @@
+package api.poja.app.repository.model;
+
+import jakarta.persistence.*;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+
+@Entity
+@Table(name = "image_submission")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class JImageSubmission {
+  @UuidGenerator @GeneratedValue @Id private UUID id;
+
+  @Column(name = "filename", nullable = false)
+  private String filename;
+
+  @Column(name = "email", nullable = false)
+  private String email;
+}
